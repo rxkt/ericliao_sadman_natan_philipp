7b78a57a3a99289341a0a35e187d9d9fed081539
CallMeSenpai <ericliao10797@hotmail.com>
2014-10-07 15:18:39 -0400

    Added a simple form lol

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request
-
 app = Flask(__name__)
+
+
 @app.route("/home")
 @app.route("/")
 def home():
     return render_template("home.html")
 
-@app.route("/search/",methods=['GET','POST'])
+@app.route("/search",methods=['GET','POST'])
 def showResults():
-    print "urmom"
     return render_template("search.html")
+
+
 if __name__ == "__main__":
     app.debug=True
     app.run()
