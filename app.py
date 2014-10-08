f0ba398d32aa3ab5a1a9a3ad0cd3a0353e5276c2
CallMeSenpai <ericliao10797@hotmail.com>
2014-10-08 16:10:14 -0400

    desu

--- app.py
+++ app.py
@@ -10,9 +10,17 @@ def home():
 @app.route("/search",methods=['GET','POST'])
 def showResults():
     results = request.args.get("input");
-    ##do stuff with results???
-    print results
-    return render_template("search.html")
+    ##utilize dictionary to create a string
+    ##based on the type of search
+    for element in request.args.keys():
+        if element=="submit":
+            ##submit mode-- display all results
+            print "submit flag"
+        elif element=="lucky":
+            ##i'm feeling lucky mode-- display the one most
+            ##important/occuring answer
+            print "lucky flag"
+    return render_template("search.html",string=results)
 
 
 if __name__ == "__main__":
